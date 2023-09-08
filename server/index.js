21c2a4676123084de524726141bd5bc327379adf
Silvinsk <81040770+Almir-git-unifc@users.noreply.github.com>
2023-09-08 19:14:13 -0300

    feat(react): v00.08.000 implements route in index.js and model in Users.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,39 @@
 const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
+const UserModel = require( './models/Users');
+
 const port = 3005;
 
-const app = express();
+// Isto vem antes do Mongoose
 app.use(cors());
 app.use(express.json());
+
+// Adaptação
+const pass = "Zbdac3r-1ex_zyk";
+const initbco = 'mongodb+srv://admin:';
+const finbco = '@cluster0.epsupyi.mongodb.net/crud?retryWrites=true&w=majority';
+const conectbco = initbco + pass + finbco;
+
+// Aqui podemos por as rotas ou endpoints ou a conexão com MongoDB
+mongoose
+.connect(
+        // Abaixo temos a string de conexão com os dados em variáveis
+        conectbco,
+       {  // abaixo temos 1 configurações padrões do Banco MongoDB
+                 useNewUrlParser: true,
+       }
+)
+
+// Aqui vamos colocar as rotas da API
+app.post('/createUser', (req, res) => {
+    UserModel.create(req.body)
+       .then(users => res.json(users))
+       .catch(err => res.json(err))
+} );
+
+
 
 // Aqui podemos por as rotas ou endpoints que é a mesma coisa; vide AA PUC 10 A1
 
